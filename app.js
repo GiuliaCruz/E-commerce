3308c6c067bc72dada2c0bf455cd4a8747968242
GiuliaCruz <giuliacruzz@gmail.com>
2023-02-25 13:34:32 -0300

    commit

---
+++ app.js
@@ -0,0 +1,7 @@
+//sticky navigation bar
+
+const header = document.querySelector("header")
+
+window.addEventListener ('scroll', function(){
+    header.classList.toggle ("sticky", this.window.scrollY > 0);
+})
